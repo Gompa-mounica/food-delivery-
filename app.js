b5a75c85160197ce1ae014dca3c8f9234cc9d540
Gompa-mounica <141259819+Gompa-mounica@users.noreply.github.com>
2024-12-20 11:00:37 +0530

    created food delivery

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+var root = document.getElementById("root");
+var mainroot = ReactDOM.createRoot(root);
+
+
+
+
+
+mainroot.render(<AppComponent/>);
